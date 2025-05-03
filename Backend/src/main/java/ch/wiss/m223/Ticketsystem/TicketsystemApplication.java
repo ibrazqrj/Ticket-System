66637b460da6bde7d2fe446a3e4ac8b9028c4431
Ibrahim Zeqiraj <153816768+ibrazqrj@users.noreply.github.com>
2025-05-03 15:08:42 +0200

    Initial commit

---
+++ Backend/src/main/java/ch/wiss/m223/Ticketsystem/TicketsystemApplication.java
@@ -0,0 +1,13 @@
+package ch.wiss.m223.Ticketsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketsystemApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/ch/wiss/m223/Ticketsystem/TicketsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.wiss.m223.Ticketsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
